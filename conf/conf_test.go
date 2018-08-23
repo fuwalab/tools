fcbbb552024301df11297fa30d5d522024efb9b8
ryotsun <ryo.tsugawa@gmail.com>
2018-08-24 01:30:35 +0900

    add unit test

--- conf/conf_test.go
+++ conf/conf_test.go
@@ -1,13 +1,42 @@
 package conf
 
-import "testing"
+import (
+	"os"
+	"strings"
+	"testing"
+)
 
-// TODO:
 func TestSetEnv(t *testing.T) {
+	SetEnv("test")
 
+	actual := os.Getenv("env")
+	expected := "test"
+
+	if actual != expected {
+		t.Errorf("Not same env.\nactual: %v, expected: %v", actual, expected)
+	}
 }
 
-// TODO:
 func TestGetAppConf(t *testing.T) {
+	config := GetAppConf()
 
+	expected := AppConf{
+		Env:    "test",
+		DBName: "test_account",
+	}
+
+	expectedProjectRootContains := "/go/src/tools"
+
+	if config.DBName != expected.DBName {
+		t.Errorf("Not same DBName.\nactual: %v, expected: %v", config.DBName, expected.DBName)
+	}
+
+	if config.Env != expected.Env {
+		t.Errorf("Not same Env.\nactual: %v, expected: %v", config.Env, expected.Env)
+	}
+
+	if !strings.Contains(config.ProjectRoot, expectedProjectRootContains) {
+		t.Errorf("Not contains path.\nactual: %v, expected: %v",
+			config.ProjectRoot, expectedProjectRootContains)
+	}
 }
